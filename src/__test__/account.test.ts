3c4ca4dda9665af4d9a0639684572aba345f99f1
fukaoi <goodakira7@gmail.com>
2020-10-23 14:24:28 +0900

    added test case: createted account, All set params that must param and optional param

--- src/__test__/account.test.ts
+++ src/__test__/account.test.ts
@@ -1,9 +1,11 @@
 import {Account} from '../account';
 import {Token} from '../token';
 import {Payment} from '../payment';
+import {Memo} from '../memo';
 
 let issuer = {pubkey: '', secret: ''}
 let receiver = {pubkey: '', secret: ''}
+let feeSource = {pubkey: '', secret: ''}
 
 describe('Stellar.Account', () => {
   beforeAll(async () => {
@@ -11,6 +13,8 @@ describe('Stellar.Account', () => {
     console.log('created issuer.', issuer);
     receiver = await Account.createTestnet();
     console.log('created receiver.', receiver);
+    feeSource = await Account.createTestnet();
+    console.log('created feeSource.', feeSource);
   })
 
 
@@ -32,12 +36,56 @@ describe('Stellar.Account', () => {
     });
   });
 
+  test('createted account, All set params that must param and optional param', async () => {
+    const createtor = await Account.createTestnet();
+    const mustParams = {
+      secret: createtor.secret,
+      startingBalance: '100',
+    }
+
+    const optinalParams = {
+      memo: Memo.text('Optional Params'),
+      feeSource: feeSource.secret,
+      feeMulti: 2,
+      timeout: 30,
+    }
+
+    const res = await Account.create(
+      mustParams.secret,
+      mustParams.startingBalance,
+    )(
+      optinalParams.memo,
+      optinalParams.feeSource,
+      optinalParams.feeMulti,
+      optinalParams.timeout,
+    );
+    expect(res).toEqual({
+      pubkey: expect.anything(),
+      secret: expect.anything()
+    });
+  });
+
   test('Get balance xlm', async () => {
     const creator = await Account.createTestnet();
     const account = await Account.create(creator.secret)();
     const res = await Account.getBalance(account.pubkey);
     expect(res.raw).toEqual('1.0000000');
     expect(res.float).toEqual('1');
+  });
+
+  test('Get balance dummy token', async () => {
+    const dummyToken = 'SPEC';
+    const token = Token.create(dummyToken, issuer.pubkey);
+    await Token.trustline(receiver.secret, token);
+    await Payment.send(
+      receiver.pubkey,
+      issuer.secret,
+      '777',
+      token,
+    )();
+    const res = await Account.getBalance(receiver.pubkey, dummyToken);
+    expect(res.raw).toEqual('777.0000000');
+    expect(res.float).toEqual('777');
   });
 
   test('Get balance dummy token', async () => {
