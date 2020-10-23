b18bdb192ee435747c8a6eb5f5a51ad38a3dae4c
fukaoi <goodakira7@gmail.com>
2020-10-23 13:12:18 +0900

    Fix comment and param

--- src/__test__/memo.test.ts
+++ src/__test__/memo.test.ts
@@ -6,12 +6,12 @@ import {
 
 describe('Stellar.Horizon', () => {
   test('set text data', () => {
-    const res = Memo.text('0'.repeat(28));
+    const res = Memo.text('A'.repeat(28));
     expect(res instanceof _Memo).toBeTruthy();
   });
 
   test('set id data', () => {
-    const res = Memo.id('9'.repeat(100));
+    const res = Memo.id('0'.repeat(100));
     expect(res instanceof _Memo).toBeTruthy();
   });
 
@@ -19,6 +19,13 @@ describe('Stellar.Horizon', () => {
     const hash = 'b36f330c6db84bef6ca34057c9739b88115565cf873fdb1350e481140a4429ad';
     const res = Memo.hash(hash);
     expect(res instanceof _Memo).toBeTruthy();
+  });
+
+  test('Set large data text to swarm ', async () => {
+    const data = 'C'.repeat(9999999);
+    const res = await Memo.Swarm.setText(data);
+    console.log(res);
+    expect(res).toBeDefined();
   });
 
   test('Get text from swarm stored data', async () => {
@@ -34,6 +41,7 @@ describe('Stellar.Horizon', () => {
     console.log(res);
     expect(res).toBeDefined();
   });
-
 });
+
+jest.setTimeout(60000);
 
