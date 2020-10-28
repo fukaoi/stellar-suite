f97dfbedcac4fa1aa73bca9572bdf3f40bc7f8e0
fukaoi <goodakira7@gmail.com>
2020-10-28 18:52:16 +0900

    added example

---
+++ examples/feeBump.js
@@ -0,0 +1,73 @@
+/*
+ * this code is integration test of production mode
+ * before exec, need to create prd-accounts.json file
+ */
+
+const {
+  Account,
+  Token,
+  Memo,
+  Payment,
+} = require('../dist/');
+
+const {
+  issuer, 
+  receiver, 
+  feeSource
+} = require('./prd-accounts.json');
+
+const createAccount = async () => {
+  return await Account.create(
+    feeSource.secret,
+    '1.5'
+  )({
+    feeSourceSecret: feeSource.secret
+  });
+}
+
+(async () => {
+  try {
+    // create account
+    if (!issuer || !receiver) {
+      issuer = await createAccount();
+      console.log('created issuer.', issuer);
+      receiver = await createAccount();
+      console.log('created receiver.', receiver);
+    }
+
+    // create token and trustline
+    const token = Token.create('TEST', issuer.pubkey);
+    await Token.trustline(receiver.secret, token);
+
+
+    // add memo
+    const data = `
+　        菅義偉首相は28日午後の衆院本会議の代表質問で、日本学術会議の会員任命拒否問題
+          を巡り、推薦に基づいて首相が任命すると定めた日本学術会議法の規定には違反しな
+          いとの認識を示した。「必ずしも推薦通りに任命しなければならないわけではない、
+          という点は政府の一貫した考え方だ」と述べた。任命拒否の理由に関し「民間出身者
+          や若手が少なく、出身や大学にも偏りが見られることも踏まえて判断した」と説明し
+          た。菅内閣発足後の本格論戦がスタートした。
+　        自身が掲げた2050年までの脱炭素社会実現に向けて「再生可能エネルギーのみならず
+          、原子力を含めたあらゆる選択肢を追求する」と語った。`;
+
+    const memo = await Memo.Swarm.setText(data);
+    console.log(memo);
+
+    // send TEST token. issuer => receiver
+    const res = await Payment.send(
+      receiver.pubkey,
+      issuer.secret,
+      '100',
+      token
+    )({
+      memo: memo,
+      feeSourceSecret: feeSource.secret
+    });
+    console.log(res.hash);
+  } catch (e) {
+    console.error(e.response.data);
+  }
+})();
+
+
